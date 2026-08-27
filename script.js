b6de5799454415bdcc151a93bbfc13891bbf4ff7
Aditya Raj <adityaninja04@gmail.com>
2026-08-27 17:25:41 +0530

    All id's are collected

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+   const form = document.querySelector('expense-form');
+   const itemName = document.querySelector('expense-name');
+   const itemAmount = document.querySelector('expense-amount');
+   const itemList = document.querySelector('expense-list');
+   const totalAmountDisplay = document.querySelector('total-amount');
+
+   
+
+
+})
