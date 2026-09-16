e29f6038d46f5d8c4caa5106bdae350224569fae
Aditya Raj <adityaninja04@gmail.com>
2026-09-16 17:06:41 +0530

    Get the value from form, created its object and pushed it into the array

--- script.js
+++ script.js
@@ -5,7 +5,25 @@ document.addEventListener('DOMContentLoaded', () => {
    const itemList = document.querySelector('expense-list');
    const totalAmountDisplay = document.querySelector('total-amount');
 
+   let expenses = [];
+
+   document.addEventListener('submit', (e)=>{
+      e.preventDefault();
+      const expenseName = itemName.value.trim();
+      const expenseAmount = parseFloat(itemAmount.value.trim()) || 0; // the problem is eventhough we had set the value of the input field as number, but the value is still being read as string, so we need to convert it to number using parseFloat or Number function.
+      if( expenseName !== "" && expenseAmount > 0 && !isNaN(expenseAmount)){
+         const objExpense = {
+            id: Date.now(),
+            name: expenseName,
+            amount: expenseAmount
+         }
+         expenses.push(objExpense);
+         saveToLocalStorage(expenses); //why didn't we stored the object in local storage? why expense array? 
+      }
+
+   });
    
+
 
 
 })
